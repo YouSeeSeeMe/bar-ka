10eb7a02d797dcced8d484448c442d0f2fe78428
wangjianfeng <wangjianfeng@soundbus.cn>
2018-04-11 21:17:34 +0800

    酒吧详情接口

--- src/common/api.js
+++ src/common/api.js
@@ -1,6 +1,7 @@
 import { domain } from '@/config'
 const api = {
-  getBarList: `${domain}/sunbar-bar-service/bar/list`
+  getBarList: `${domain}/sunbar-bar-service/bar/list`,
+  BarDetail: `${domain}/sunbar-bar-service/bar/`,
 }
 
 export default api
